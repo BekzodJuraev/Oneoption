1fdb03ba3ca083231f33d7b0ee7b448c643ed0bd
Nurbek Juraev <powerzver98@gmail.com>
2024-07-23 23:20:01 +0500

    refactoring code

--- backend/signals.py
+++ backend/signals.py
@@ -9,15 +9,14 @@ from django.dispatch import receiver
 @receiver(post_save,sender=Profile)
 def create_profile_for_user(sender,instance,created,*args,**kwargs):
     if created:
-        Referral.objects.create(profile=instance, referral_type='doxod')
-        Referral.objects.create(profile=instance, referral_type='oborot')
-        Referral.objects.create(profile=instance, referral_type='sub')
+        Referral.objects.get_or_create(profile=instance, referral_type='doxod')
+        Referral.objects.get_or_create(profile=instance, referral_type='oborot')
+        Referral.objects.get_or_create(profile=instance, referral_type='sub')
     if instance.recommended_by and hasattr(instance.recommended_by, 'profile') and instance.deposit > 0:
-        try:
-            FTD.objects.get(profile=instance)
-        except FTD.DoesNotExist:
-            FTD.objects.create(
-                profile=instance,
-                recommended_by=instance.recommended_by.profile,
-                ftd=instance.deposit
-            )
+        FTD.objects.get_or_create(
+            profile=instance,
+            defaults={
+                'recommended_by': instance.recommended_by.profile,
+                'ftd': instance.deposit
+            }
+        )

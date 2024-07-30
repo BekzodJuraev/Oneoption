6ab2fb83f9f1a1ca836832ec62506d581036c9de
Bekzod <powerzver98@gmail.com>
2024-07-31 00:33:43 +0500

    add static

--- oneoption/settings.py
+++ oneoption/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -156,7 +156,7 @@ SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/google/complete/'  # Redirect to this URL aft
 SOCIAL_AUTH_LOGIN_ERROR_URL = '/'
 
 
-
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [

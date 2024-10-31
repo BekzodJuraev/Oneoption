c0ee2d4729c27f231dc7bd540c85967f0232d32b
Bekzod <powerzver98@gmail.com>
2024-10-31 11:43:06 +0500

    added commission 10 % sub ref

--- backend/management/commands/ftd.py
+++ backend/management/commands/ftd.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 import logging
 from django.db.models import F
 import time
+from decimal import Decimal
 from django.db import transaction
 from backend.models import FTD,Profile
 class Command(BaseCommand):
@@ -13,11 +14,76 @@ class Command(BaseCommand):
         while True:
             try:
                 today = timezone.now()
-                if today.date().day == 28:
+                if today.date().weekday() == 0:
+
+                    profiles = Profile.objects.select_related('recommended_by_partner').all()
+                    updates=[]
+                    partners=[]
+
+                    for profile in profiles:
+                        total=profile.income_oborot + profile.income_doxod
+                        profile.total_income=F('total_income') + total
+                        profile.income_oborot=0
+                        profile.income_doxod=0
+
+                        if profile.recommended_by_partner:
+                            partner = profile.recommended_by_partner
+                            partner.total_income = F('total_income') + total * Decimal('0.1')
+                            partners.append(partner)
+
+
+                        updates.append(profile)
+
+                    with transaction.atomic():
+                        if updates:
+                            Profile.objects.bulk_update(updates, ['total_income', 'income_oborot', 'income_doxod'])
+
+                        if partners:
+                            Profile.objects.bulk_update(partners, ['total_income'])
+
+
+
+
+
+
+                    # profiles.update(
+                    #     total_income=F('total_income') + F('income_oborot') + F('income_doxod'),
+                    #     income_oborot=0,
+                    #     income_doxod=0
+                    #
+                    # )
+
+
+
+                    # profiles = Profile.objects.select_related('recommended_by_partner').annotate(
+                    #     partner_income=F('total_income') * Decimal('0.1')
+                    # )
+                    # updates=[]
+                    #
+                    # for profile in profiles:
+                    #     if profile.recommended_by_partner:
+                    #         partner = profile.recommended_by_partner
+                    #         partner.total_income = F('total_income') + profile.partner_income
+                    #         updates.append(partner)
+                    #
+                    # print(updates)
+                    #
+                    #
+                    # if updates:
+                    #     with transaction.atomic():
+                    #         Profile.objects.bulk_update(updates, ['total_income'])
+
+
+
+
+                if today.date().day == 1:
                     last_month = today.month - 1 if today.month > 1 else 12
 
                     profiles = Profile.objects.all()
                     updates = []
+
+
+
 
 
                     ftd_counts = {
@@ -31,9 +97,7 @@ class Command(BaseCommand):
                         next_level = profile.next_level
 
 
-                        profile.total_income = F('total_income') + F('income_oborot') + F('income_doxod')
-                        profile.income_oborot = 0
-                        profile.income_doxod = 0
+
 
 
                         if ftd > 299:
@@ -48,6 +112,10 @@ class Command(BaseCommand):
                         elif ftd > 49:
                             new_level = 2
                             next_level = 100
+                        else:
+                            new_level = 1
+                            next_level = 50
+
 
                         if new_level != profile.level and next_level != profile.next_level:
                             profile.level = new_level
@@ -75,13 +143,13 @@ class Command(BaseCommand):
                     if updates:
                         with transaction.atomic():
                             Profile.objects.bulk_update(updates,
-                                                        ['total_income', 'income_oborot', 'income_doxod', 'level',
-                                                         'next_level'])
+                                                        ['level','next_level'])
 
 
             except Exception as e:
                 print(e)
                 logger.error(f"Error updating profile {e}")
+
 
 
 
